efcbbbd81b1fd4f6bcee450e518ea3a22de2e6f1
Thomas Dalgetty <tdalgetty0213@gmail.com>
2024-09-24 04:18:26 +0000

    finish US3

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,5 @@
 class Student < ApplicationRecord
-  validates :school_email, uniqueness: true
+  has_one_attached :profile_picture
+  validates :first_name, :last_name, :major, :graduation_date, presence: true
+  validates :school_email, format: {with: /[A-Za-z0-9]+@msudenver\.edu/, message: "Must be a valid msudenver.edu email"}, uniqueness: true, presence: true
 end
