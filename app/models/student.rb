344d7cf13c1f7ffaa22c78ef4c8a8252fcf33477
Thomas Dalgetty <tdalgetty0213@gmail.com>
2024-10-09 20:15:22 +0000

    implement search by major

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,11 @@
 class Student < ApplicationRecord
+  VALID_MAJORS = ["Computer Engineering BS", 
+                  "Computer Information Systems BS", 
+                  "Computer Science BS",
+                  "Cybersecurity BS",
+                  "Data Science and Machine Learning BS" ]
   has_one_attached :profile_picture
   validates :first_name, :last_name, :major, :graduation_date, presence: true
   validates :school_email, format: {with: /[A-Za-z0-9]+@msudenver\.edu/, message: "Must be a valid msudenver.edu email"}, uniqueness: true, presence: true
+  validates :major, inclusion: { in: VALID_MAJORS, message:"%{value} is not a valid major" }
 end
